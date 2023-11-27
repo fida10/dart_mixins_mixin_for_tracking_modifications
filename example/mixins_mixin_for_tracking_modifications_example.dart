e80dd039d1ba84181b0eeb3e666194d984c239fb
eshad <fidaeshad10@gmail.com>
2023-11-27 12:41:12 -0600

    first commit

---
+++ example/mixins_mixin_for_tracking_modifications_example.dart
@@ -0,0 +1,6 @@
+import 'package:mixins_mixin_for_tracking_modifications/mixins_mixin_for_tracking_modifications.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
